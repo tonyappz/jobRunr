c350bc297515675d587b6cd5bc34fc7611dadeb0
Tony Ryu <tonyappz@gmail.com>
2022-08-02 13:37:12 +0900

    Initial commit

---
+++ src/main/java/com/tonyappz/jobRunr/JobRunrApplication.java
@@ -0,0 +1,13 @@
+package com.tonyappz.jobRunr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRunrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRunrApplication.class, args);
+	}
+
+}
